2d8bb2344626079e7af67d7bf20aaba279afe212
cuesta <s0943020@gm.ncue.edu.tw>
2022-10-31 01:35:44 +0800

    fix unexpected arg

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def test():
         user=os.getenv("USERNAME"),
         passwd=os.getenv("PASSWORD"),
         db=os.getenv("DATABASE"),
-        ssl_mode="VERIFY_IDENTITY",
+        # ssl_mode="VERIFY_IDENTITY",
         ssl={
             "ca": "/etc/ssl/cert.pem"
         }
